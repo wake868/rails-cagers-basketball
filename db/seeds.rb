ed760c0a1c1f52b3f8c3d7051638afff81e328e6
wake868 <wake868@gmail.com>
2022-03-24 18:52:10 +0000

    added database seed records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,82 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+articles = Article.create([
+  {
+    title: "Schedules",
+    slug: "schedules",
+    rank: 1,
+    body: "<p>⁠Cagers Basketball team practice schedules. Tourney schedules and links can also be found here.</p><p></p>",
+    blurb: "Cagers Basketball team practice schedules. Tourney schedules and links can also be found here."
+  },
+  {
+    title: "Clark County Today Article",
+    slug: "clark-county-today-article",
+    rank: 2,
+    body: "<p></p><iframe\n          width=\"560\"\n          height=\"300\"\n          frameBorder=\"0\"\n          allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"\n          allowFullScreen\n          title=\"https://www.youtube.com/embed/7RyEp3JKcMY\"\n          src=\"https://www.youtube.com/embed/7RyEp3JKcMY\"\n        ></iframe><p><br>Family coaching background provided the passion that Edwards uses to help Clark County hoopsters.</p><p><a target='_blank' title=\"http://www.clarkcountytoday.com/sports/greg-edwards-cagers-basketball-helps-area-youth-prepare-for-high-school-play/#.W70302hKjZu\" href=\"http://www.clarkcountytoday.com/sports/greg-edwards-cagers-basketball-helps-area-youth-prepare-for-high-school-play/#.W70302hKjZu\"><u><strong>Click here to read the full article.</strong></u></a></p><p></p>",
+    blurb: "Family coaching background provided the passion that Edwards uses to help Clark County hoopsters."
+  },
+  {
+    title: "Philosophy & Goals",
+    slug: "philosophy-and-goals",
+    rank: 3,
+    body: "<p>Cagers Basketball was created to provide competitive basketball programs for players in grades 3-12, who have a love for the game and the desire to continue to work to become the best that they can be in the game. Each separate basketball program offered fulfills a need in the development of each individual player, based on age, skill level and physical ability.</p><p>Teaching proper fundamental technique in regard to each skill; whether offensive or defensive and helping each player to understand the tremendous benefit and importance in being grounded first in fundamentals of the game is a primary goal of Cagers Basketball.</p><p>A second primary goal the program is to help each individual player develop a sense of commitment, work ethic, and to gain an understanding of just how much time and effort one must put in to become the best that they can be in the game. A players goal should not be to become the next Michael Jordan, Larry Bird, Kobe Bryant or Lebron James, it should be to become the best individual and team player that you can become, within the limits of your physical abilities. However to reach this goal you may have to spend an equal amount of time and effort as any of the aforementioned pros did when they were growing up, and learning the game.</p><p>This basketball program is definitely here to serve the kids. For them to enjoy the game, learn how to play the right way and continue to improve, so as to reach their full potential.</p><p>The question is: <strong>&quot;WHAT ARE THE PLAYERS PREPARED TO DO?&quot;</strong></p>",
+    blurb: "Cagers Basketball was created to provide competitive basketball programs for players in grades 3-12, who have a love for the game and the desire to continue to work to become the best that they can be in the game."
+  },
+  {
+    title: "Program Director",
+    slug: "program-director",
+    rank: 4,
+    body: "<p>Cagers Basketball is under the direction of program director Greg Edwards. Coach Edwards comes from an excellent coaching background and sees his role first and foremost as a teacher of the game. <strong>&quot;I believe you teach in practice each day and you ‘coach’ in games. If you do a great job teaching, your players and teams will play as close to their potential as is physically possible under game conditions.&quot;</strong></p><p>Coach Edwards father was a very successful college coach at the Division one level for 25 years, and his uncle, Rollad Todd was the first head coach of the Portland Trailblazers, as well as coaching at the college level for more than 35 years. Coach Edwards has been a varsity coach for 15 years and has helped lead those teams to 14 state playoff appearances in 15 seasons. He is the all-time winning-est coach in school history at Hillsboro High School of Oregon, and has an overall high school record of 220-95. This basketball Academy is an important goal in his coaching life.</p><p><strong>&quot;It is a great privilege and opportunity to get to work with and help so many young players of all ages who have a passion for this great game, and have the desire to continue to learn and the willingness to work to keep getting better, so they so they may become the best they can be in the game.&quot;</strong></p><p>My intention is to build a foundation for a solid program that is able to serve the many young players in the Salmon Creek community, and southwest Washington area for years to come.</p>",
+    blurb: "Cagers Basketball is under the direction of program director Greg Edwards. Coach Edwards comes from an excellent coaching background and sees his role first and foremost as a teacher of the game."
+  },
+  {
+    title: " Select Youth Teams",
+    slug: "select-youth-teams",
+    rank: 5,
+    body: "<p>Cagers Basketball offers a competitive youth program for players in grades 4-8.</p><p><strong>WHAT WE OFFER:</strong></p><ul><li><div><strong>⁠</strong>A winter season which runs November 7th thru March 7th for young players in grades 4-8.</div></li><li><div>A spring season for players in grades 5-8, runs March 15th - May 15th.</div></li><li><div>passing</div></li><li><div>This program is for the competitive player who one day aspires to play on his respective high school team. This is not a rec program, and requires a reasonable commitment of each player to his team.</div></li></ul><p><br>Players in the Columbia River High School and Skyview High School districts are the main focus of the program because of both gym location and the goal of having a continued feeder program for the young players who will be the future stars at Columbia River and Skyview High Schools.</p><ul><li><div>This program provides and excellent balance between the number of practices and games to ensure proper player development.</div></li><li><div>Our teams get a minimum of 3 practices a week and play 24-30 games during the winter season.</div></li></ul><p></p>",
+    blurb: "Cagers Basketball offers a competitive youth program for players in grades 4-8. This program is for the competitive player who one day aspires to play on his respective high school team."
+  },
+  {
+    title: "Skill Development",
+    slug: "skill-development",
+    rank: 6,
+    body: "<p>Cagers Basketball offers individual and group basketball training sessions. Players are grouped by age and skill level. The focus of each session is the continued improvement of each individual’s offensive fundamentals.</p><p><strong>SKILLS COVERED AREA AS FOLLOWS:</strong></p><ul><li><div><strong>⁠</strong>proper footwork</div></li><li><div>ball handling</div></li><li><div>passing</div></li><li><div>shooting technique</div></li><li><div>shooting off the dribble</div></li><li><div>shooting(moving without the ball)</div></li><li><div>coming off screens</div></li><li><div>one-on-one skills</div></li><li><div>post play-both facing and back to the basket</div></li><li><div>how to split a double team</div></li><li><div>how to score against a quicker defender</div></li><li><div>how to score against a taller defender</div></li><li><div>how to execute a proper shot fake</div></li><li><div>proper free throw fundamentals</div></li></ul><p> <strong>BASIC DEFENSIVE TECHNIQUE:</strong></p><ul><li><div>stance</div></li><li><div>footwork</div></li><li><div>proper balance and positioning</div></li><li><div>on ball and off the ball defense</div></li></ul><p><strong><br>INDIVIDUAL TRAINING SESSION DATES:</strong> April, May, June, July, August, September and October.</p><p>This program is a must for any young player who wishes to learn and develop the essential skills needed to play at the high school level. Remember this - In 2010 over 65 freshmen were cut at Skyview and Columbia River - High schools alone. Parents need to understand that every high school has only one freshman team and can keep only 12 players. Too many kids simply come into high school not prepared, because they have not played enough and have not learned the proper fundamentals. As a high school varsity coach, I stress daily the essential skills both offensively, and defensively that should be mastered if you wish to, not only play in high school, but to excel at a high level.</p><p><strong>CAGERS IS A GREAT PROGRAM TO HELP ANY YOUNG PLAYER REACH THAT GOAL</strong></p><p><strong>FOR MORE INFORMATION ON THIS PROGRAM CALL PROGRAM DIRECTOR </strong></p><p><strong>GREG EDWARDS AT (503) 735-5552</strong></p>",
+    blurb: "Cagers Basketball offers individual and group basketball training sessions. Players are grouped by age and skill level."
+  },
+  {
+    title: "Basketball Camps",
+    slug: "basketball-camps",
+    rank: 7,
+    body: "<p>Cagers Basketball will run several weeks of basketball camp for boys grades 3-12. Each camp is limited to 40 players a week or session so that all players get more repetition and individual instruction. The camps are offensive oriented, and teach all aspects of offensive play.</p><p>Cagers Basketball Camps are a great way to improve skills and stay sharp in the off-season. The camps are also very reasonably priced, allowing many young players to attend more than one week of camp.</p><p>Above all, we want players to have fun playing the game while learning the essential fundamentals that will help them continue to improve. Each player attending camp will learn a practice plan that they can take home with them and use as a blueprint for getting better when they go out on their own to practice.</p><p></p>",
+    blurb: "Cagers Basketball will run several weeks of basketball camp for boys grades 3-12. Each camp is limited to 40 players a week or session so that all players get more repetition and individual instruction."
+  },
+  {
+    title: "Coaching Clinics",
+    slug: "coaching-clinics",
+    rank: 8,
+    body: "<p>CAGERS BASKETBALL ACADEMY WILL OFFER FALL COACHING CLINICS FOR ANY YOUTH COACHES WANTING TO LEARN MORE ABOUT HOW TO TEACH AND COACH YOUTH BASKETBALL.</p><p>The clinics are a must attend event for Hot Shot coaches and any beginner coach working with kids grades 2-8. The clinic will cover all fundamental aspects of man-to-man defense and offensive play and will demonstrate the drills you can use to teach these skills.</p><p>​The clinics are broken up into two 2-1⁄2 hour sessions, with defensive fundamentals covered in the first session and offensive fundamentals covered in the second session.</p><p></p><p>QUESTIONS?<br><strong>CALL COACH EDWARDS AT: (503) 735-5552</strong></p><p></p>",
+    blurb: "The clinics are broken up into two 2-1⁄2 hour sessions, with defensive fundamentals covered in the first session and offensive fundamentals covered in the second session."
+  },
+  {
+    title: "Strength Shoe Training",
+    slug: "strength-shoe-training",
+    rank: 9,
+    body: "<p>This program combines fundamental basketball drills with the use of the strength shoe to greatly improve both athletic performance and basketball skill level. This program is second to none in improving quickness, jumping ability, and raw speed. The strength shoes utilize plyometric construction to target calf muscle training, building mass and definition, while increasing quantity of fast twitch muscle fibers. Results include increase in vertical jump, reactive leg strength jumping skill, body coordination and range of motion. Average results of athletes who have completed this program are as follows</p><p><strong>BENEFITS:</strong></p><ul><li><div>Increase vertical jump 5 to 10 inches</div></li><li><div>⁠Lower 40 yard dash time by 2/10th of a second</div></li><li><div>Increase reactive strength, jumping skill and coordination</div></li><li><div>Create maximum power output</div></li><li><div>Increase fast twitch muscle fibers</div></li><li><div>Increase calf mass and definition</div></li><li><div>Increase range of motion</div></li><li><div>Improved basketball skill level through the training of this program</div></li></ul><p></p><p>If you are a serious athlete, or a parent of an athlete, the strength shoe basketball training program will give you or your child the tools to prepare for the highest level of competition.</p><p>Current St. Anthony High School coach, Bob Hurley, who recently won his 1000th game, was inducted into the basketball hall of fame in 2010 (One of only two high school coaches in history to be included) and winner of 26 New Jersey State high school state championships says this about the Strength Shoe basketball program.</p><blockquote><em><strong>&quot;We have used the Strength shoe training program at St. Anthony’s since 1989, and in over 40 years of coaching, I have never seen a training program that improves a players quickness and jumping ability like the Strength Shoe does. I’m amazed that more kids and basketball programs do not utilize the strength shoe, in training athletes, especially when you can combine a basketball workout with a strength shoe workout to accomplish two goals at once. Improve basketball skill level and improved athletic ability.</strong></em></blockquote><blockquote><em><strong>Many professionals in the strength training field have approached me over the years with different methods to increase strength, speed, quickness and jumping ability, however I have yet to see another training method that yields the results that the strength shoe system produces in out athletes.&quot;</strong></em></blockquote><blockquote><em><strong>-Bob Hurley, St. Anthony’s Jersey City, NJ</strong></em></blockquote><p></p><p>Cagers Basketball will offer 3 strength shoe training sessions per year, each session is 3 days a week for a 9 week period as prescribed by strength systems to achieve maximum results. Space per session is limited to 24 players.</p><ul><li><div><strong>Cagers can order the shoes for you. When filling out the registration form, simply indicate whether you need a pair of shoes and what size.</strong></div></li></ul><p></p>",
+    blurb: "This program combines fundamental basketball drills with the use of the strength shoe to greatly improve both athletic performance and basketball skill level. "
+  },
+  {
+    title: "Photos",
+    slug: "photos",
+    rank: 10,
+    body: "<p>Various photos from Cagers Basketball games.</p>",
+    blurb: "Various photos from Cagers Basketball games."
+  },
+  {
+    title: "Contact Information",
+    slug: "contact-information",
+    rank: 11,
+    body: "<p>For additional information regarding Cagers Basketball please contact coach Edwards.</p><p><strong>COACH EDWARDS: (503) 735-5552</strong></p><iframe\n          width=\"450\"\n          height=\"350\"\n          frameBorder=\"0\"\n          allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\"\n          allowFullScreen\n          title=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2785.3905878882165!2d-122.67078598420665!3d45.72326122414017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495ad95a0134943%3A0xb288534895705f42!2s14010+NE+3rd+Ct%2C+Vancouver%2C+WA+98685!5e0!3m2!1sen!2sus!4v1526421103545\"\n          src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2785.3905878882165!2d-122.67078598420665!3d45.72326122414017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495ad95a0134943%3A0xb288534895705f42!2s14010+NE+3rd+Ct%2C+Vancouver%2C+WA+98685!5e0!3m2!1sen!2sus!4v1526421103545\"\n        ></iframe><p></p>",
+    blurb: "Contact information for coach Edwards."
+  }
+])
